57ff2f0e97f5430da0680feb2d4d802546c0d3d5
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-06-29 23:30:35 +0530

    Restructured Packages Folder

--- apps/server/src/routers/project.routes.ts
+++ apps/server/src/routers/project.routes.ts
@@ -1,11 +1,23 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
 
-import type { SuccessResponse } from "@productify/types";
+import type { SuccessResponse } from "@productify/shared/types";
+import { ProjectSchema } from "@productify/shared/schemas/project";
 
-export const projectRoutes = new Hono().get("/", async (c) => {
-  return c.json<SuccessResponse<{ id: number }[]>>({
-    message: "Project Fetched",
-    success: true,
-    data: [{ id: 1 }, { id: 2 }, { id: 3 }],
+export const projectRoutes = new Hono()
+  .get("/", async (c) => {
+    return c.json<SuccessResponse<{ id: number }[]>>({
+      message: "Project Fetched",
+      success: true,
+      data: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    });
+  })
+  .post("/", zValidator("json", ProjectSchema), async (c) => {
+    const body = c.req.valid("json");
+
+    return c.json<SuccessResponse<{ id: number; name: string }>>({
+      message: "Project Created",
+      success: true,
+      data: { id: 1, name: body.name },
+    });
   });
-});
