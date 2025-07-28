b32de59e3c42f8f4c13d60674508e272ce4dc195
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-07-28 22:30:19 +0530

    base nestjs setup

---
+++ apps/api/src/app.module.ts
@@ -0,0 +1,19 @@
+import {
+	type MiddlewareConsumer,
+	Module,
+	type NestModule,
+} from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { LoggerMiddleware } from "./middlewares/logger.middleware";
+
+@Module({
+	imports: [],
+	controllers: [AppController],
+	providers: [AppService],
+})
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(LoggerMiddleware).forRoutes("/*path");
+	}
+}
---
+++ apps/api/src/middlewares/logger.middleware.ts
@@ -0,0 +1,31 @@
+import { Injectable, Logger, type NestMiddleware } from "@nestjs/common";
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+	private readonly logger = new Logger("HTTP");
+
+	use(req: FastifyRequest, res: FastifyReply, next: () => void) {
+		const { method, originalUrl } = req;
+		const start = Date.now();
+
+		this.logger.log(`Incoming Request: ${method} ${originalUrl}`);
+
+		// Try to attach finish event on raw response or fallback to res
+		const rawRes = res.raw || res;
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+		rawRes.on
+			? rawRes.on("finish", () => {
+					const responseTime = Date.now() - start;
+					this.logger.log(
+						`Request ${method} ${originalUrl} completed in ${responseTime}ms`,
+					);
+				})
+			: this.logger.warn(
+					"Response raw stream not available to measure finish event",
+				);
+
+		next();
+	}
+}
