4eb0b5f332c0608fa8a6beb7a5f5b4179866f85c
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-07-28 22:34:30 +0530

    add formatting and linting

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,39 +1,39 @@
 import { ConsoleLogger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import {
-	FastifyAdapter,
-	type NestFastifyApplication,
+  FastifyAdapter,
+  type NestFastifyApplication,
 } from "@nestjs/platform-fastify";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-	const fastifyAdapter = new FastifyAdapter();
+  const fastifyAdapter = new FastifyAdapter();
 
-	const app = await NestFactory.create<NestFastifyApplication>(
-		AppModule,
-		fastifyAdapter,
-		{
-			logger: new ConsoleLogger({
-				colors: true,
-			}),
-		},
-	);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    fastifyAdapter,
+    {
+      logger: new ConsoleLogger({
+        colors: true,
+      }),
+    },
+  );
 
-	app.useGlobalPipes(
-		new ValidationPipe({
-			transform: true,
-			whitelist: true,
-		}),
-	);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
-	app.enableCors({
-		origin: ["http://localhost:3000"],
-		credentials: true,
-	});
-	app.setGlobalPrefix("api/v1");
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  });
+  app.setGlobalPrefix("api/v1");
 
-	const port = process.env.PORT ?? 3000;
-	await app.listen(port, "0.0.0.0");
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, "0.0.0.0");
 }
 void bootstrap();
--- apps/api/src/middlewares/logger.middleware.ts
+++ apps/api/src/middlewares/logger.middleware.ts
@@ -3,29 +3,29 @@ import type { FastifyReply, FastifyRequest } from "fastify";
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-	private readonly logger = new Logger("HTTP");
+  private readonly logger = new Logger("HTTP");
 
-	use(req: FastifyRequest, res: FastifyReply, next: () => void) {
-		const { method, originalUrl } = req;
-		const start = Date.now();
+  use(req: FastifyRequest, res: FastifyReply, next: () => void) {
+    const { method, originalUrl } = req;
+    const start = Date.now();
 
-		this.logger.log(`Incoming Request: ${method} ${originalUrl}`);
+    this.logger.log(`Incoming Request: ${method} ${originalUrl}`);
 
-		// Try to attach finish event on raw response or fallback to res
-		const rawRes = res.raw || res;
+    // Try to attach finish event on raw response or fallback to res
+    const rawRes = res.raw || res;
 
-		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-		rawRes.on
-			? rawRes.on("finish", () => {
-					const responseTime = Date.now() - start;
-					this.logger.log(
-						`Request ${method} ${originalUrl} completed in ${responseTime}ms`,
-					);
-				})
-			: this.logger.warn(
-					"Response raw stream not available to measure finish event",
-				);
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    rawRes.on
+      ? rawRes.on("finish", () => {
+          const responseTime = Date.now() - start;
+          this.logger.log(
+            `Request ${method} ${originalUrl} completed in ${responseTime}ms`,
+          );
+        })
+      : this.logger.warn(
+          "Response raw stream not available to measure finish event",
+        );
 
-		next();
-	}
+    next();
+  }
 }
