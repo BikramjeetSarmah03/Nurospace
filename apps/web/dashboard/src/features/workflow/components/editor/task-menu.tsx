6daecb589732b97e7c73fcc97222c3520ce0c1cc
Bikramjeet Sarmah <83879699+BikramjeetSarmah03@users.noreply.github.com>
2025-08-16 20:59:00 +0530

    added resizable task menu

--- apps/web/dashboard/src/features/workflow/components/editor/task-menu.tsx
+++ apps/web/dashboard/src/features/workflow/components/editor/task-menu.tsx
@@ -1,33 +1,78 @@
+import { ChevronRightIcon } from "lucide-react";
+import { useState } from "react";
+
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-import { TASK_TYPE, type TaskType } from "../../lib/constants/task";
-import { TaskRegistry } from "../registry/task/registery";
 import { Button } from "@/components/ui/button";
 
-export default function TaskMenu() {
-  return (
-    <aside className="bg-white p-2 px-4 border-l-2 w-[340px] min-w-[340px] max-w-[340px] h-full overflow-auto border-separate">
-      <Accordion
-        type="multiple"
-        className="w-full"
-        defaultValue={["extraction"]}
-      >
-        <AccordionItem value="extraction">
-          <AccordionTrigger className="font-bold">
-            Data extraction
-          </AccordionTrigger>
+import {
+  TASK_TYPE,
+  type TaskType,
+} from "@/features/workflow/lib/constants/task";
 
-          <AccordionContent className="flex flex-col gap-1">
-            {Object.values(TASK_TYPE).map((type) => (
-              <TaskMenuBtn taskType={type} key={type} />
-            ))}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+import { TaskRegistry } from "../registry/task/registery";
+
+import { cn } from "@/lib/utils";
+
+export default function TaskMenu() {
+  const [open, setOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <aside
+      className={cn(
+        "bg-white border-l-2 h-full overflow-auto border-separate",
+        "relative overflow-hidden",
+        "transition-[width] duration-300",
+        open ? "w-[340px]" : "w-10",
+      )}
+    >
+      <div className={cn("p-2 px-4", open && "border-b")}>
+        <button
+          type="button"
+          className={cn(
+            "top-0 left-0 absolute bg-gray-200 p-2 border cursor-pointer",
+            open && "p-2.5",
+          )}
+          onClick={toggleOpen}
+        >
+          <ChevronRightIcon
+            className={cn(!open ? "rotate-180" : "")}
+            size={18}
+          />
+        </button>
+
+        {open && <h1 className="text-center">Tasks Menu</h1>}
+      </div>
+
+      <div className="px-4">
+        {open && (
+          <Accordion
+            type="multiple"
+            className="w-full"
+            defaultValue={["extraction"]}
+          >
+            <AccordionItem value="extraction">
+              <AccordionTrigger className="font-bold">
+                Data extraction
+              </AccordionTrigger>
+
+              <AccordionContent className="flex flex-col gap-1">
+                {Object.values(TASK_TYPE).map((type) => (
+                  <TaskMenuBtn taskType={type} key={type} />
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        )}
+      </div>
     </aside>
   );
 }
